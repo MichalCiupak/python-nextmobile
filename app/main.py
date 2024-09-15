e66ed4eebda019639efd0fdc2e95ebd0ee72020d
MichalCiupak <michalciupak.26@gmail.com>
2024-09-15 13:42:42 +0200

    Fix database initialization

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ import uvicorn
 from .api import app
 from .database import Base, engine
 
-if __name__ == "__main__":
+@app.on_event("startup")
+async def startup_event():
     Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
